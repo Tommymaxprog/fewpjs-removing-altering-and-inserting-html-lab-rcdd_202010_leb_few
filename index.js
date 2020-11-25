ebb12f79789d4a433d1cd73daea7915d650adfb9
sagacious-shell-7968 <tommysytc.tc@gmail.com>
2020-11-25 09:53:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
 // Write your code here!
-let element= document.createElement('div');
-document.body.appendChild(element);
-let ul = document.createElement('ul')
- 
-for (let i = 0; i < 3; i++) {
-  let li = document.createElement('li')
-  li.innerHTML = (i + 1).toString()
-  ul.appendChild(li)
-}
- 
-element.appendChild(ul);
-element.style.backgroundColor = '#27647B';
-element.style.textAlign = 'center';
-ul.style.textAlign = 'left';
-ul.removeChild(ul.querySelector('li:nth-child(2)'));
-ul.remove()
+
 main.remove();
 let newHeader=document.createElement('h1#victory');
 newHeader.innerHTML="YOUR-NAME is the champion";
