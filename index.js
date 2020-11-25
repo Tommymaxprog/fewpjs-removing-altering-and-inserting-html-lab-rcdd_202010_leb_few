7cba36bfa835b359b39cbb8f19ea8f4f0ea324a2
sagacious-shell-7968 <tommysytc.tc@gmail.com>
2020-11-25 10:01:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 main.remove();
 let newHeader=document.createElement('h1');
 document.body.appendChild(newHeader);
-document.getElementByTagName('h1').Id="victory";
+newHeader.Id="victory";
 newHeader.innerHTML="YOUR-NAME is the champion";
